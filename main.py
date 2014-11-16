58b6c351eaf73757cb4c4b46d2f53aca473c0bc4
Navjot Panesar] <NavjotSPanesar@gmail.com>
2014-11-15 21:14:12 -0500

    Added a simple analytic

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,14 +14,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from google.appengine.ext import ndb
 import webapp2
 from google.appengine.ext import db
+import json
 import datetime
+
 
 class MainHandler(webapp2.RequestHandler):
     def post(self):
         self.handleTweetInfo()
+
+    def get(self):
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+
+        dataRequest = self.request.get("request", "")
+        if dataRequest == "time summary":
+            self.response.write(self.getTimeSummary())
+        else:
+            self.response.write("hi")
+
+    def getTimeSummary(self):
+        q = Tweet.all()
+        sum_command_lengths = dict()
+        command_tweet_count = dict()
+        for p in q.run():
+            print p
+            if p.command not in sum_command_lengths:
+                sum_command_lengths[p.command] = int(p.timeTaken)
+            else:
+                sum_command_lengths[p.command] += int(p.timeTaken)
+
+            if p.command not in command_tweet_count:
+                command_tweet_count[p.command] = 1
+            else:
+                command_tweet_count[p.command] += 1
+
+        avg_command_lengths = dict()
+        for command, t in sum_command_lengths.iteritems():
+            avg_command_lengths[command] = (t / command_tweet_count[command])
+        return json.dumps(avg_command_lengths)
+
 
     def handleTweetInfo(self):
         token = self.request.get("token")
@@ -61,6 +93,7 @@ class Tweet(db.Model):
     timeTaken = db.IntegerProperty(indexed=False)
     date = db.DateTimeProperty(auto_now_add=True)
 
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+                                  ('/', MainHandler)
+                              ], debug=True)
